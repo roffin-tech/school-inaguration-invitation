56e337d9ded8f99466430e97ca3fc5866ec6a32c
Roffin Varghese <roffin@techgentsia.com>
2025-02-25 12:20:29 +0530

    title update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,16 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  ssr: true
-})
+  ssr: true,
+  app: {
+    head: {
+      title:
+        process.env.PROJECT_TITLE ||
+        `It is with great joy and excitement that we warmly invite you to the grand inauguration of the new building at St. Joseph's High School, Kulathoor. Your presence will make the occasion truly special!`,
+      titleTemplate: '%s',
+      charset: 'UTF-8',
+      viewport: 'width=device-width, initial-scale=1.0',
+      script: [],
+    },
+  },
+});
